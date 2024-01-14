8e4a96859e7cc88fa45d3910196a44f63a4ec925
Ayush Singhal <ayushsinghal998@gmail.com>
2024-01-14 22:06:01 +0530

    added handler and get payments api

--- controller/transaction.js
+++ controller/transaction.js
@@ -1,13 +1,8 @@
 const db = require("../config/db");
-const TRANSACTION_STATUS = require("../utils/constants/transactionStatus");
-const InvalidRequest = require("../errors/invalidRequest");
-const ServerFailure = require("../errors/serverFailure");
-const e = require("express");
+const { completeTransaction } = require("./handlers/payment");
 
 const User = db.User;
-const Account = db.Account;
 const Transaction = db.Transaction;
-const sequelize = db.sequelize;
 
 async function startPayment(req, res) {
   try {
@@ -30,88 +25,28 @@ async function startPayment(req, res) {
   }
 }
 
-async function completeTransaction(sender, receiver, amount) {
-  let transaction = createNewTransaction(sender, receiver, amount);
+async function findUserPayments(req, res) {
   try {
-    await makePayment(sender, receiver, amount);
-    transaction.status = TRANSACTION_STATUS.processed;
+    const transactions = await Transaction.findAll({
+      where: {
+        senderId: req.params.userId,
+      },
+    });
+
+    if (!transactions) {
+      return res.status(200).json([]);
+    }
+
+    return res.status(200).json(transactions);
   } catch (err) {
-    transaction.status = TRANSACTION_STATUS.failed;
-
-    if (err instanceof InvalidRequest || err instanceof ServerFailure) {
-      transaction.failureMessage = err.message;
-    } else {
-      transaction.failureMessage =
-        err.message.length < 50 ? err.message : "Unknow Error.";
-    }
+    return res.status(500).json({
+      message: "Error while fetching transactions.",
+      error: err.message,
+    });
   }
-
-  return await transaction.save();
-}
-
-async function makePayment(sender, receiver, amount) {
-  await sequelize.transaction(async (transaction) => {
-    const senderAccount = await findAccountByUser(sender, transaction);
-    const receiverAccount = await findAccountByUser(receiver, transaction);
-
-    validatePaymentDetails(senderAccount, receiverAccount, amount);
-
-    await updateAccountBalance(senderAccount, -amount, transaction);
-    await updateAccountBalance(receiverAccount, amount, transaction);
-  });
-}
-
-function createNewTransaction(sender, receiver, amount) {
-  return Transaction.build({
-    senderId: sender.id,
-    receiverId: receiver.id,
-    amount: amount,
-    status: TRANSACTION_STATUS.pending,
-  });
-}
-
-function validatePaymentDetails(senderAccount, recieverAccount, amount) {
-  if (!senderAccount) {
-    throw new InvalidRequest("Sender does not have a account.");
-  } else if (!recieverAccount) {
-    throw new InvalidRequest("Reciever does not have a account.");
-  } else if (senderAccount.balance - amount < 0) {
-    throw new InvalidRequest("Sender balance is insufficient.");
-  }
-}
-
-async function findAccountByUser(user, transaction) {
-  return await Account.findOne(
-    {
-      where: { userId: user.id },
-    },
-    { transaction }
-  );
-}
-
-async function updateAccountBalance(account, amount, transaction) {
-  const accountId = account.id;
-  const currentBalance = parseFloat(account.balance);
-  const newBalance = currentBalance + parseFloat(amount);
-
-  const [_, affectedRowsCount] = await Account.update(
-    { balance: newBalance },
-    {
-      where: { id: accountId, balance: currentBalance },
-      returning: true,
-      transaction,
-    }
-  );
-
-  if (affectedRowsCount !== 1) {
-    throw new ServerFailure("Please retry after sometime..");
-  }
-
-  console.log(
-    `Account balance updated successfully. Account ID: ${accountId}, New Balance: ${newBalance}`
-  );
 }
 
 module.exports = {
   startPayment,
+  findUserPayments,
 };
--- routes/transaction.js
+++ routes/transaction.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const transaction = require("../controller/transaction");
 
 router.post("", transaction.startPayment);
+router.get("/:userId", transaction.findUserPayments);
 
 module.exports = router;
